630507b477ca9283652c1486b8feb4b7b0617d8b
Bo Lorentsen <bl@moch.dk>
2013-07-04 15:44:35 +0200

    config update

--- www/site_config.js
+++ www/site_config.js
@@ -1,14 +1,15 @@
 // Setup for a single site, use things like apache alias
 // to replace this file per installation
 var dojoConfig = {
-    debug: false,
+    debug: true,
     async: true,
     locale: 'da',
     keep_locale: true,
     domain_name: 'essens',  // This is the only thing needed to be replaced in a new installation
-    service_uri: 'https://phoenix.moch.dk/service',
+    service_uri: null,
     parseOnLoad: false,
-    sse_events: true,
+    sse_events: false,
+    auto_login: false,
     packages: [
         {
             name: 'kso',
